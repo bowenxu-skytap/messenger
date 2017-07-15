7e4172cc8361878e485a11c399031b20eed39f5f
rippleberg <jimmyxbw@gmail.com>
2017-07-15 15:28:15 -0400

    Messenger App
    JAX-RS learning

---
+++ src/main/java/com/bowen/rest/messenger/exception/DataNotFoundException.java
@@ -0,0 +1,15 @@
+package com.bowen.rest.messenger.exception;
+
+public class DataNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5147189670806163147L;
+
+	public DataNotFoundException(String message) {
+		super(message);
+	}
+	
+	
+}
---
+++ src/main/java/com/bowen/rest/messenger/exception/GenericExceptionMapper.java
@@ -0,0 +1,21 @@
+package com.bowen.rest.messenger.exception;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import com.bowen.rest.messenger.model.ErrorMessage;
+
+//@Provider
+public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
+
+	@Override
+	public Response toResponse(Throwable exception) {
+		ErrorMessage errorMessage = new ErrorMessage(exception.getMessage(), 500, "www.bowen.com");
+		return Response.status(Status.INTERNAL_SERVER_ERROR)
+						.entity(errorMessage)
+						.build();
+	}
+	
+}
